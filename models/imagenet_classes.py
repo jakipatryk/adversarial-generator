036ac57d3cf59f61482a6394eb4b4473aebfcaee
jakipatryk <18032838+jakipatryk@users.noreply.github.com>
2021-01-06 16:23:41 +0100

    [models] Fix opening file when module is imported

--- models/imagenet_classes.py
+++ models/imagenet_classes.py
@@ -1,6 +1,8 @@
 """Contains dictionary and list of all classes (1000) from ImageNet dataset."""
 
 import json
+import os
 
-IMAGENET_CLASSES_DICT = json.load(open("data/imagenet_class_index.json"))
+IMAGENET_CLASSES_DICT = json.load(
+    open(os.path.join(os.path.dirname(__file__), "data/imagenet_class_index.json")))
 IMAGENET_CLASSES = list(map(lambda x: x[1], IMAGENET_CLASSES_DICT.values()))
